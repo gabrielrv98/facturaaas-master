5b1805e69a3023f6e3afaa3c83069434f6096679
minimerlyn <gabrielrv98@gmail.com>
2019-12-27 14:06:42 +0100

    Primera version de entidad de Factura
    No se que esta bien y que no, solo adapte otra clase.
    
    -Falta: obetener la forma de pago el usuario.

---
+++ src/main/java/es/uvigo/esei/dagss/facturaaas/entidades/Factura.java
@@ -0,0 +1,207 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.uvigo.esei.dagss.facturaaas.entidades;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "FACTURA")
+public class Factura implements Serializable{
+    
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;//hace referencia a NUMERODEFACTURA
+
+    private String ejercicio;
+
+    private String cliente;
+
+    private String fecha;
+    
+    @ManyToOne
+    private String formaDePago;//es la del usuario ac activo
+
+    @Enumerated(EnumType.STRING)
+    private EstadoFactura estado;
+    
+    private String comentarios;
+
+    public Factura() {
+    }
+
+    public Factura(Long id, String ejercicio, String cliente, String fecha,
+                        String formaDePago) {
+        this.id = id;
+        this.ejercicio = ejercicio;
+        this.cliente = cliente;
+        this.fecha = fecha;
+        this.formaDePago = formaDePago;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEjercicio() {
+        return ejercicio;
+    }
+
+    public void setEjercicio(String ejercicio) {
+        this.ejercicio = ejercicio;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getFormaDePago() {
+        return formaDePago;
+    }
+
+    public void setFormaDePago(String formaDePago) {
+        this.formaDePago = formaDePago;
+    }
+
+    public EstadoFactura getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoFactura estado) {
+        this.estado = estado;
+    }
+
+    public String getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(String comentarios) {
+        this.comentarios = comentarios;
+    }
+
+
+    //ni idea si hace falta
+    @Override
+    public int hashCode() {
+        if (id != null) {
+            return Objects.hashCode(this.id);
+        } else {
+            return hashCodePorContenido();
+        }
+    }
+
+        //ni idea si hace falta
+
+    @Override
+    public boolean equals(Object obj) {
+        if (id != null) {
+            return equalsPorId(obj);
+        } else {
+            return equalsPorContenido(obj);
+        }
+    }
+
+        //ni idea si hace falta
+    public int hashCodePorContenido() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.id);
+        hash = 67 * hash + Objects.hashCode(this.ejercicio);
+        hash = 67 * hash + Objects.hashCode(this.cliente);
+        hash = 67 * hash + Objects.hashCode(this.fecha);
+        hash = 67 * hash + Objects.hashCode(this.formaDePago);
+        hash = 67 * hash + Objects.hashCode(this.estado);
+        hash = 67 * hash + Objects.hashCode(this.comentarios);
+        return hash;
+    }
+
+    public boolean equalsPorContenido(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Factura other = (Factura) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.ejercicio, other.ejercicio)) {
+            return false;
+        }
+        if (!Objects.equals(this.cliente, other.cliente)) {
+            return false;
+        }
+        if (!Objects.equals(this.fecha, other.fecha)) {
+            return false;
+        }
+        if (!Objects.equals(this.formaDePago, other.formaDePago)) {
+            return false;
+        }
+        if (this.estado != other.estado) {
+            return false;
+        }
+        if (!Objects.equals(this.comentarios, other.comentarios)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean equalsPorId(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Factura other = (Factura) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Factura{" + "id=" + id + ", ejercicio=" + ejercicio + ", cliente=" + cliente + ", fecha=" + fecha 
+                + ", formaDePago=" + formaDePago + ", estado=" + estado + ", comentarios=" + comentarios +'}';
+    }
+}
