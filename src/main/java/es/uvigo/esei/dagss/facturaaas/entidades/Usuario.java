5d86e4487192d05a01f972d9d6101efc6c083688
minimerlyn <gabrielrv98@gmail.com>
2020-01-20 17:07:21 +0100

    .

--- src/main/java/es/uvigo/esei/dagss/facturaaas/entidades/Usuario.java
+++ src/main/java/es/uvigo/esei/dagss/facturaaas/entidades/Usuario.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Objects;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
